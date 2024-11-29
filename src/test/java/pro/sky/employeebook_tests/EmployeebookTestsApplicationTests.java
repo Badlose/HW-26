6d7146e8824fcb3007973c94364bef7f7a3cbd74
Badlose <git@github.com:Badlose/helloworld.git>
2024-11-29 22:01:58 +0300

    Initial commit

---
+++ src/test/java/pro/sky/employeebook_tests/EmployeebookTestsApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.employeebook_tests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeebookTestsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
